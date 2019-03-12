bc716639a25066df849c8d152576173c2fa7986f
adrienvaucard <adrien.vaucard@gmail.com>
2019-03-12 10:37:56 +0100

    Add driver and driverModel

---
+++ models/driver.js
@@ -0,0 +1,68 @@
+const { mongoose } = require('../db/mongoose.js');
+
+const Driver = mongoose.model('Driver', {
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    size: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    speedGnd: {
+        type: Number,
+        required: true
+    },
+    speedWtr: {
+        type: Number,
+        required: true
+    },
+    speedAir: {
+        type: Number,
+        required: true
+    },
+    speedGty: {
+        type: Number,
+        required: true
+    },
+    acceleration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    handlingGnd: {
+        type: Number,
+        required: true
+    },
+    handlingWtr: {
+        type: Number,
+        required: true
+    },
+    handlingAir: {
+        type: Number,
+        required: true
+    },
+    handlingGty: {
+        type: Number,
+        required: true
+    },
+    grip: {
+        type: Number,
+        required: true
+    },
+    miniturbo: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true
+    }
+})
+
+module.exports = { Driver };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { User } = require('./models/user');
+const { User } = require('./models/driver');
 const ObjectID = mongoose.Types.ObjectId;
 
 const app = express();
