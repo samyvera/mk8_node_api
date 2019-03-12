c7054988963514e167d54c7bf049a7dc4cfecc67
samy <samy_leni@hotmail.fr>
2019-03-12 10:56:36 +0100

    get

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { User } = require('./models/user');
+const { Driver } = require('./models/user');
 const ObjectID = mongoose.Types.ObjectId;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/user', (req, res) => new User({ email:req.body.email }).save().then(user => res.send(user)).catch(err => res.status(500).send(err)));
+/*
+model
+GET model
+GET model/id
+POST model
+DELETE model/id
+PUT model/id
+*/
 
-app.get('/user', (req, res) => User.find((err, users) => res.json(users)));
+//driver
+app.get('/driver', (req, res) => Driver.find(drivers).then(drivers => res.json(drivers)).catch(err, res.status(500).send(err)));
 
-app.get('/user/:id', (req, res) => ObjectID.isValid(req.params.id) ? User.findById(req.params.id).then(user => user ? res.json(user) : res.status(404).send()) : res.status(404).send());
+app.get('/driver/:id', (req, res) => ObjectID.isValid(req.params.id) ? Driver.findById(req.params.id).then(driver => driver ? res.json(driver) : res.status(404).send()) : res.status(404).send());
 
-app.delete('/user/:id', (req, res) => ObjectID.isValid(req.params.id) ? User.findByIdAndRemove(req.params.id).then(user => user ? res.json(user) : res.status(404).send()) : res.status(404).send());
+app.post('/driver', (req, res) => new Driver({ name:req.body.name, size:req.body.size, driverModel:req.body.driverModel }).save().then(driver => res.send(driver)).catch(err => res.status(500).send(err)));
+
+app.delete('/driver/:id', (req, res) => ObjectID.isValid(req.params.id) ? Driver.findByIdAndRemove(req.params.id).then(driver => driver ? res.json(driver) : res.status(404).send()) : res.status(404).send());
 
 const port = yargs.argv.port && !isNaN(yargs.argv.port) && yargs.argv.port > 0 && !(yargs.argv.port % 1) ? yargs.argv.port : 3000;
 app.listen(port, () => console.log("Server listen on port ", port));
