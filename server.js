5c9300a93a492237080be3c3a5eb35a13e5825af
samy <samy_leni@hotmail.fr>
2019-03-12 11:54:27 +0100

    fix types

--- server.js
+++ server.js
@@ -10,79 +10,91 @@ const { Glider } = require('./models/glider');
 const { Tire } = require('./models/tire');
 const ObjectID = mongoose.Types.ObjectId;
 
-const bodyType = {
-    name: req.body.name,
-    speedGnd: req.body.speedGnd,
-    speedWtr: req.body.speedWtr,
-    speedAir: req.body.speedAir,
-    speedGty: req.body.speedGty,
-    acceleration: req.body.acceleration,
-    weight: req.body.weight,
-    handlingGnd: req.body.handlingGnd,
-    handlingWtr: req.body.handlingWtr,
-    handlingAir: req.body.handlingAir,
-    handlingGty: req.body.handlingGty,
-    grip: req.body.grip,
-    miniturbo: req.body.miniturbo
+const type = (req, type) => {
+    if (type === "bodyType") {
+        return {
+            name: req.body.name,
+            speedGnd: req.body.speedGnd,
+            speedWtr: req.body.speedWtr,
+            speedAir: req.body.speedAir,
+            speedGty: req.body.speedGty,
+            acceleration: req.body.acceleration,
+            weight: req.body.weight,
+            handlingGnd: req.body.handlingGnd,
+            handlingWtr: req.body.handlingWtr,
+            handlingAir: req.body.handlingAir,
+            handlingGty: req.body.handlingGty,
+            grip: req.body.grip,
+            miniturbo: req.body.miniturbo
+        };
+    }
+    else if (type === "gliderType") {
+        return {
+            name: req.body.name,
+            speedGnd: req.body.speedGnd,
+            speedWtr: req.body.speedWtr,
+            speedAir: req.body.speedAir,
+            speedGty: req.body.speedGty,
+            acceleration: req.body.acceleration,
+            weight: req.body.weight,
+            handlingGnd: req.body.handlingGnd,
+            handlingWtr: req.body.handlingWtr,
+            handlingAir: req.body.handlingAir,
+            handlingGty: req.body.handlingGty,
+            grip: req.body.grip,
+            miniturbo: req.body.miniturbo
+        };
+    }
+    else if (type === "tireType") {
+        return {
+            name: req.body.name,
+            speedGnd: req.body.speedGnd,
+            speedWtr: req.body.speedWtr,
+            speedAir: req.body.speedAir,
+            speedGty: req.body.speedGty,
+            acceleration: req.body.acceleration,
+            weight: req.body.weight,
+            handlingGnd: req.body.handlingGnd,
+            handlingWtr: req.body.handlingWtr,
+            handlingAir: req.body.handlingAir,
+            handlingGty: req.body.handlingGty,
+            grip: req.body.grip,
+            miniturbo: req.body.miniturbo
+        };
+    }
+    else if (type === "driverType") {
+        return {
+            name:req.body.name,
+            size:req.body.size,
+            driverModel:req.body.driverModel
+        }
+    }
+    else if (type === "driverModelType") {
+        return {
+            name: req.body.name,
+            size:req.body.size,
+            speedGnd: req.body.speedGnd,
+            speedWtr: req.body.speedWtr,
+            speedAir: req.body.speedAir,
+            speedGty: req.body.speedGty,
+            acceleration: req.body.acceleration,
+            weight: req.body.weight,
+            handlingGnd: req.body.handlingGnd,
+            handlingWtr: req.body.handlingWtr,
+            handlingAir: req.body.handlingAir,
+            handlingGty: req.body.handlingGty,
+            grip: req.body.grip,
+            miniturbo: req.body.miniturbo,
+            active: req.body.catch
+        }
+    }
 };
-const gliderType = {
-    name: req.body.name,
-    speedGnd: req.body.speedGnd,
-    speedWtr: req.body.speedWtr,
-    speedAir: req.body.speedAir,
-    speedGty: req.body.speedGty,
-    acceleration: req.body.acceleration,
-    weight: req.body.weight,
-    handlingGnd: req.body.handlingGnd,
-    handlingWtr: req.body.handlingWtr,
-    handlingAir: req.body.handlingAir,
-    handlingGty: req.body.handlingGty,
-    grip: req.body.grip,
-    miniturbo: req.body.miniturbo
-};
-const tireType = {
-    name: req.body.name,
-    speedGnd: req.body.speedGnd,
-    speedWtr: req.body.speedWtr,
-    speedAir: req.body.speedAir,
-    speedGty: req.body.speedGty,
-    acceleration: req.body.acceleration,
-    weight: req.body.weight,
-    handlingGnd: req.body.handlingGnd,
-    handlingWtr: req.body.handlingWtr,
-    handlingAir: req.body.handlingAir,
-    handlingGty: req.body.handlingGty,
-    grip: req.body.grip,
-    miniturbo: req.body.miniturbo
-};
-const driverType = {
-    name:req.body.name,
-    size:req.body.size,
-    driverModel:req.body.driverModel
-}
-const driverModelType = {
-    name: req.body.name,
-    size:req.body.size,
-    speedGnd: req.body.speedGnd,
-    speedWtr: req.body.speedWtr,
-    speedAir: req.body.speedAir,
-    speedGty: req.body.speedGty,
-    acceleration: req.body.acceleration,
-    weight: req.body.weight,
-    handlingGnd: req.body.handlingGnd,
-    handlingWtr: req.body.handlingWtr,
-    handlingAir: req.body.handlingAir,
-    handlingGty: req.body.handlingGty,
-    grip: req.body.grip,
-    miniturbo: req.body.miniturbo,
-    active: req.body.catch
-}
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/body', (req, res) => new Body(bodyType).save().then(body => res.send(body)).catch(err => res.status(500).send(err)));
+app.post('/body', (req, res) => new Body(type("bodyType")).save().then(body => res.send(body)).catch(err => res.status(500).send(err)));
 
 /*
 model
